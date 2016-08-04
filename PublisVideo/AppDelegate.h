eb947018cd8ad9b69e6a80f4be019b2328c5059d
wolianw003 <wolianw003@wolianw003deimac.local>
2016-08-04 14:38:11 +0800

    Initial Commit

---
+++ PublisVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PublisVideo
+//
+//  Created by wolianw003 on 16/8/4.
+//  Copyright © 2016年 wolianw003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PublisVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PublisVideo
+//
+//  Created by wolianw003 on 16/8/4.
+//  Copyright © 2016年 wolianw003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
